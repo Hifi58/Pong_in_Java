554292aeac600e5518fc7151347084193d9963a3
Hifi58 <gillet.tony@gmail.com>
2023-03-22 14:06:59 +0100

    adding a game frame

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,23 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class Main {
+public class Main{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Set new Game Frame
+		JFrame gameFrame = new JFrame("Pong");
+		gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// Frame Dimension
+		gameFrame.setSize(1000,650);
+		gameFrame.setResizable(false);
+		
+		// Frame Design
+		gameFrame.getContentPane().setBackground(Color.DARK_GRAY);
+		
+		// Frame Visibility
+		gameFrame.setVisible(true);
+		
 
 	}
 
